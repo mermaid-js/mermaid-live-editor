7527af522d803d50a55837c7e03269adf73dfa0a
Sidharth Vinod <sidharthv96@gmail.com>
2021-05-17 12:14:00 +0530

    Add more presets

--- src/lib/components/preset.svelte
+++ src/lib/components/preset.svelte
@@ -69,7 +69,35 @@
           ORDER ||--|{ ORDER-ITEM : includes
           PRODUCT-CATEGORY ||--|{ PRODUCT : contains
           PRODUCT ||--o{ ORDER-ITEM : "ordered in"
-            `
+            `,
+		'Git Graph': `gitGraph:
+options
+{
+    "nodeSpacing": 150,
+    "nodeRadius": 10
+}
+end
+commit
+branch newbranch
+checkout newbranch
+commit
+commit
+checkout master
+commit
+commit
+merge newbranch
+
+            `,
+		'User Journey': `  journey
+    title My working day
+    section Go to work
+      Make tea: 5: Me
+      Go upstairs: 3: Me
+      Do work: 1: Me, Cat
+    section Go home
+      Go downstairs: 5: Me
+      Sit down: 3: Me
+      `
 	};
 
 	const loadSampleDiagram = (diagramType: string): void => {
