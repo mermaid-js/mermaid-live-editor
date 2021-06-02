b4f5a9814ab30667fe9e23d622c1481156fb1c5c
Sidharth Vinod <sidharthv96@gmail.com>
2021-06-02 11:33:20 +0530

    Test hook

--- src/lib/components/actions.svelte
+++ src/lib/components/actions.svelte
@@ -28,6 +28,7 @@
 			canvas.width = userimagesize * ratio;
 			canvas.height = userimagesize;
 		}
+
 		const context = canvas.getContext('2d');
 		context.fillStyle = 'white';
 		context.fillRect(0, 0, canvas.width, canvas.height);
