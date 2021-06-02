4c4427a4b9a0055a1fe26deabd85ac2b864f0bf2
Sidharth Vinod <sidharthv96@gmail.com>
2021-06-03 02:03:43 +0530

    Touchup

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,14 +8,7 @@ module.exports = {
 		'prettier'
 	],
 	plugins: ['svelte3', 'tailwindcss', '@typescript-eslint'],
-	ignorePatterns: [
-		'docs/*',
-		'*.cjs',
-		'snapshots.js',
-		'svelte.config.js',
-		'package.json',
-		'README.md'
-	],
+	ignorePatterns: ['docs/*', '*.cjs', '*.md', 'snapshots.js', 'svelte.config.js', 'package.json'],
 	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
 	settings: {
 		'svelte3/typescript': () => require('typescript')
