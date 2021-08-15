2fdfab56b3cf2509af9804dfdbbdcbfa850adaae
sidharthv96 <sidharthv96@users.noreply.github.com>
2021-08-15 18:49:14 +0000

    deploy: be81fab7ae344e363367ac10103a179b9bac5cce

---
+++ beta/_app/chunks/theme-e58fe9d5.js
@@ -0,0 +1,1 @@
+import{J as e,K as s,C as t}from"./vendor-4cf3bda8.js";const a=e(t({isDark:!1,theme:"light"}),s(),"themeStore"),o=["dark","synthwave","halloween","forest","luxury","black","dracula"],r=e=>{const s=o.includes(e);console.log("Setting theme",e),a.set({theme:e,isDark:s})};export{r as s,a as t};
