91fe1911fb015a3be63756a5a6c5ccaed766b346
Sidharth Vinod <sidharthv96@gmail.com>
2021-09-17 18:43:40 +0530

    Update src/lib/components/navbar.svelte
    Co-authored-by: Yash Singh <saiansh2525@gmail.com>

--- src/lib/components/navbar.svelte
+++ src/lib/components/navbar.svelte
@@ -61,7 +61,7 @@
 		<ul class="lg:flex items-center justify-between text-base pt-4 lg:pt-0">
 			{#each links as { title, href, icon }}
 				<li>
-					<a class="btn btn-ghost " target="_blank" {href}>
+					<a class="btn btn-ghost" target="_blank" {href}>
 						{#if icon}
 							<i class={icon} />
 						{/if}
