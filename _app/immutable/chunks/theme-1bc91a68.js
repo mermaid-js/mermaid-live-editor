31a8449eff091b6e829035ac86ffe671a5e3943a
sidharthv96 <sidharthv96@users.noreply.github.com>
2022-07-08 12:36:51 +0000

    deploy: 57df1cfac103aa5bf6720d389daa9098e9ecd19e

---
+++ _app/immutable/chunks/theme-1bc91a68.js
@@ -0,0 +1,1 @@
+import{w as t}from"./preload-helper-803ef6f6.js";import{p as o,a as r}from"./state-3abd2121.js";const e=o(t({isDark:!1}),r(),"themeStore"),l=["dark","synthwave","halloween","aqua","forest","luxury","black","dracula"],n=s=>{s.includes(" ")&&(s=s.split(" ")[1].trim());const a=l.includes(s);console.log("Setting theme",s),e.set({theme:s,isDark:a})};export{n as s,e as t};
