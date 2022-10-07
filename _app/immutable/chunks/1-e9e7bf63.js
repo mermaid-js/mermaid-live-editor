4521031b182081bd2f804ffc27ca4f57951d7169
sidharthv96 <sidharthv96@users.noreply.github.com>
2022-10-07 11:14:29 +0000

    deploy: 4c22921e4e621bea6606f9c395d2595692bfd946

---
+++ _app/immutable/chunks/1-e9e7bf63.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-4f05386d.js";export{t as component};
---
+++ _app/immutable/chunks/2-59dafa27.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-760c626d.js";export{t as component};
