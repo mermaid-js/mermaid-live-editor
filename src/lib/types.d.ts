f794fa9e0ef2c3de816895d4c93abc38f86c398e
Sidharth Vinod <sidharthv96@gmail.com>
2022-11-25 13:25:20 +0530

    chore: cleanup

--- src/lib/types.d.ts
+++ src/lib/types.d.ts
@@ -1,11 +1,3 @@
-/**
- * Can be made globally available by placing this
- * inside `global.d.ts` and removing `export` keyword
- */
-export interface Locals {
-  userid: string;
-}
-
 export interface MarkerData {
   severity: number;
   message: string;
