8c07649b10c2e278f2b3fb8e0df4ca0b8fcdf0ba
Sidharth Vinod <sidharthv96@gmail.com>
2023-02-16 15:48:02 +0530

    Remove mindmap

--- src/lib/util/mermaid.ts
+++ src/lib/util/mermaid.ts
@@ -1,11 +1,4 @@
 import mermaid, { type MermaidConfig } from 'mermaid';
-import mindmap from '@mermaid-js/mermaid-mindmap';
-
-const initialize = mermaid.registerExternalDiagrams([mindmap]);
-
-export const init = async () => {
-  await initialize;
-};
 
 export const render = async (
   config: MermaidConfig,
@@ -15,13 +8,9 @@ export const render = async (
 ): Promise<void> => {
   // Should be able to call this multiple times without any issues.
   mermaid.initialize(config);
-  // console.log('Rendering', code);
-  // mermaid.mermaidAPI.render(id, code, callback);
-  await init();
   await mermaid.mermaidAPI.renderAsync(id, code, callback);
 };
 
 export const parse = async (code: string): Promise<boolean> => {
-  await init();
   return await mermaid.parseAsync(code);
 };
