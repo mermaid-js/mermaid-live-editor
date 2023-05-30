b2b284c1185295e2a3ed5a23a35d7062f8d1d641
Subhash Halder <halder.subhash@gmail.com>
2023-05-30 21:42:17 +0530

    Added quadrantChart in the live editor with sample

--- src/lib/components/Preset.svelte
+++ src/lib/components/Preset.svelte
@@ -101,7 +101,21 @@
             Argument mapping
     Tools
       Pen and paper
-      Mermaid`
+      Mermaid`,
+    QuadrantChart: `quadrantChart
+    title Reach and engagement of campaigns
+    x-axis Low Reach --> High Reach
+    y-axis Low Engagement --> High Engagement
+    quadrant-1 We should expand
+    quadrant-2 Need to promote
+    quadrant-3 Re-evaluate
+    quadrant-4 May be improved
+    Campaign A: [0.3, 0.6]
+    Campaign B: [0.45, 0.23]
+    Campaign C: [0.57, 0.69]
+    Campaign D: [0.78, 0.34]
+    Campaign E: [0.40, 0.34]
+    Campaign F: [0.35, 0.78]`
   };
 
   type SampleTypes = keyof typeof samples;
@@ -114,7 +128,7 @@
   };
 
   // Adding in this array will add an icon to the preset menu
-  const newDiagrams: SampleTypes[] = ['Mindmap'];
+  const newDiagrams: SampleTypes[] = ['Mindmap', 'QuadrantChart'];
   const diagramOrder: SampleTypes[] = [
     'Sequence',
     'Flow',
@@ -125,7 +139,8 @@
     'User Journey',
     'Git',
     'Pie',
-    'Mindmap'
+    'Mindmap',
+    'QuadrantChart'
   ];
 </script>
 
