11a814ac7d754d18afa75016bdd4f97fc6b4bd03
Max Stanley <github@maxstanley.uk>
2023-11-21 21:00:14 +0000

    Add environment arguments to Dockerfile

--- src/env.d.ts
+++ src/env.d.ts
@@ -3,9 +3,6 @@
 interface ImportMetaEnv {
   readonly MERMAID_RENDERER_URL?: string;
   readonly MERMAID_KROKI_RENDERER_URL?: string;
-  readonly MERMAID_CDN_URL?: string;
-  readonly MERMAID_BASE_URL?: string;
-  readonly MERMAID_LOCAL?: boolean;
   // more env variables...
 }
 
--- src/lib/util/env.ts
+++ src/lib/util/env.ts
@@ -1,9 +1,4 @@
 export const env = {
   rendererUrl: import.meta.env.MERMAID_RENDERER_URL ?? 'https://mermaid.ink',
-  krokiRendererUrl: import.meta.env.MERMAID_KROKI_RENDERER_URL ?? 'https://kroki.io',
-  mermaidCDNUrl: import.meta.env.MERMAID_CDN_URL ?? 'https://unpkg.com/@mermaid-js',
-  mermaidBaseURL: import.meta.env.MERMAID_BASE_URL ?? 'http://localhost:9000',
-  useLocalMermaid: import.meta.env.MERMAID_LOCAL ?? false,
-  isDev: import.meta.env.DEV,
-  baseURL: import.meta.env.BASE_URL
+  krokiRendererUrl: import.meta.env.MERMAID_KROKI_RENDERER_URL ?? 'https://kroki.io'
 };
