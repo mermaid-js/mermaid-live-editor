50b0d12353d575302fec1784c795dcd6398a0975
dontry <mccoy018@gmail.com>
2024-03-05 17:30:17 +1100

    Replace zenuml example with a more sophisticated one

--- src/lib/components/Preset.svelte
+++ src/lib/components/Preset.svelte
@@ -143,10 +143,27 @@
     style m fill:#d6d,stroke:#333,stroke-width:4px
     `,
     ZenUML: `zenuml
-    title Sync message
-    A.SyncMessage
-    A.SyncMessage(with, parameters) {
-      B.nestedSyncMessage()
+    title Order Service
+    @Actor Client #FFEBE6
+    @Boundary OrderController #0747A6
+    @EC2 <<BFF>> OrderService #E3FCEF
+    group BusinessService {
+      @Lambda PurchaseService
+      @AzureFunction InvoiceService
+    }
+
+    @Starter(Client)
+    // \`POST /orders\`
+    OrderController.post(payload) {
+      OrderService.create(payload) {
+        order = new Order(payload)
+        if(order != null) {
+          par {
+            PurchaseService.createPO(order)
+            InvoiceService.createInvoice(order)      
+          }      
+        }
+      }
     }
     `
   };
