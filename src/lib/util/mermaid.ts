664d81f6eaa5f1222297efe7f4af7fb59424b1ad
dontry <mccoy018@gmail.com>
2024-03-12 21:54:48 +1100

    Refactor diagram registration without top-level promise

--- src/lib/util/mermaid.ts
+++ src/lib/util/mermaid.ts
@@ -2,17 +2,15 @@ import mermaid from 'mermaid';
 import type { MermaidConfig, RenderResult } from 'mermaid';
 import zenuml from '@mermaid-js/mermaid-zenuml';
 
-try {
-  await mermaid.registerExternalDiagrams([zenuml]);
-} catch (error) {
-  console.error('Error registering external diagrams:', error);
-}
+const init = mermaid.registerExternalDiagrams([zenuml]);
 
 export const render = async (
   config: MermaidConfig,
   code: string,
   id: string
 ): Promise<RenderResult> => {
+  await init;
+
   // Should be able to call this multiple times without any issues.
   mermaid.initialize(config);
   return await mermaid.render(id, code);
