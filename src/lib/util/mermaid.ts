51456270077581560aae204bd5208033c5815207
Miguel <thepoderoso72@gmail.com>
2025-01-24 18:27:31 +0000

    chance to add your own logos, idk if it works yet

--- src/lib/util/mermaid.ts
+++ src/lib/util/mermaid.ts
@@ -6,6 +6,44 @@ import mermaid from 'mermaid';
 mermaid.registerLayoutLoaders(elkLayouts);
 const init = mermaid.registerExternalDiagrams([zenuml]);
 
+function UrlsToRegisterObject(UrlOb){
+    let name = UrlOb.name;
+    let url = UrlOb.url;
+    return {
+        name: name,
+        loader: () => import(url).then((module) => module.icons),
+    }
+}
+
+function loadInputs() {
+    const data = JSON.parse(document.getElementById('extension-data').innerText);
+    //extension-data doesn't exist by itself, but should exist if the extension is installed
+    /*
+    data has the shape:
+    [
+        {
+            url: part of url for lazy loading of icons (example: @iconify-json/logos)
+            name: 'some name all minus no spaces no numbers, just lower case lyrics'
+        }
+    ]
+    */
+    return data;
+}
+
+function mermaidRegisterProcess(){
+    let inputs = loadInputs();
+    if (inputs){
+        mermaid.registerIconPacks(
+            loadInputs().map(UrlsToRegisterObject)
+        )
+    }
+    return undefined;
+}
+
+mermaidRegisterProcess();
+
+
+
 export const render = async (
   config: MermaidConfig,
   code: string,
