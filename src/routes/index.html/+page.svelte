cdc7f9faa4d3748d276822f216be5d3253d76319
Sidharth Vinod <sidharthv96@gmail.com>
2025-02-25 08:50:03 +0530

    fix: Add redirect for /index.html

---
+++ src/routes/index.html/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import { goto } from '$app/navigation';
+  import { base } from '$app/paths';
+  import { onMount } from 'svelte';
+
+  onMount(async () => {
+    await goto(`${base}/edit`, {
+      replaceState: true
+    });
+  });
+</script>
