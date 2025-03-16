fe428c7ceabf12fe69acd0cf06573f2fad0fa949
Sidharth Vinod <sidharthv96@gmail.com>
2025-03-15 19:55:50 -0700

    chore: Remove redundant config

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
-import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-static';
+import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -12,13 +12,8 @@ const config = {
   ],
   kit: {
     adapter: adapter({
-      pages: `docs`
-    }),
-    paths: process.env['DEPLOY']
-      ? {
-          base: `/mermaid-live-editor`
-        }
-      : {}
+      pages: 'docs'
+    })
   }
 };
 
