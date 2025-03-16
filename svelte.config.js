9288094c30161331463f135b1e29583573ff09c0
Sidharth Vinod <sidharthv96@gmail.com>
2025-03-15 20:00:13 -0700

    chore: Update import

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,12 @@
 import adapter from '@sveltejs/adapter-static';
-import preprocess from 'svelte-preprocess';
+import { sveltePreprocess } from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
   preprocess: [
-    preprocess({
+    sveltePreprocess({
       postcss: true
     })
   ],
