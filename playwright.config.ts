87e0b915439aab836842d2f133fff7e14ca6b0fe
Sidharth Vinod <sidharthv96@gmail.com>
2025-03-17 19:51:04 -0700

    chore: Fix pw issues

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,8 +5,8 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
-  reporter: 'html',
+  workers: process.env.CI ? 3 : undefined,
+  reporter: process.env.CI ? [['github'], ['list']] : 'list',
   use: {
     baseURL: 'http://localhost:3000',
     trace: 'on-first-retry',
