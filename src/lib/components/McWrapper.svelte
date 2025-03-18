d039f6534af6214c844c3084e83e2a4af5d4db0c
Sidharth Vinod <sidharthv96@gmail.com>
2025-03-18 10:51:08 -0700

    feat: Disable mermaidchart on unsupported diagrams

--- src/lib/components/McWrapper.svelte
+++ src/lib/components/McWrapper.svelte
@@ -1,24 +1,40 @@
 <script lang="ts">
   import { env } from '$/util/env';
+  import { stateStore } from '$/util/state';
   import * as Tooltip from '$lib/components/ui/tooltip';
   import type { Snippet } from 'svelte';
   import ExternalLinkIcon from '~icons/material-symbols/open-in-new-rounded';
 
-  let { children }: { children: Snippet } = $props();
+  let {
+    children,
+    shouldCheckDiagramType = true
+  }: { children: Snippet; shouldCheckDiagramType?: boolean } = $props();
+
+  let shouldDisableComponent = $derived(
+    shouldCheckDiagramType && $stateStore.diagramType === 'zenuml'
+  );
 </script>
 
 {#if env.isEnabledMermaidChartLinks}
   <Tooltip.Provider>
     <Tooltip.Root delayDuration={100}>
       <Tooltip.Trigger>
-        {@render children()}
+        <div class={[shouldDisableComponent && 'pointer-events-none cursor-not-allowed grayscale']}>
+          {@render children()}
+        </div>
       </Tooltip.Trigger>
       <Tooltip.Content side="bottom">
         <div class="flex items-center gap-2">
-          <ExternalLinkIcon />
-          <span class="flex items-center gap-1">
-            Opens a new tab in <div class="text-accent">MermaidChart.com</div>
-          </span>
+          {#if shouldDisableComponent}
+            <div class="text-muted-foreground">
+              This diagram type is not supported in MermaidChart.com
+            </div>
+          {:else}
+            <ExternalLinkIcon />
+            <span class="flex items-center gap-1">
+              Opens a new tab in <div class="text-accent">MermaidChart.com</div>
+            </span>
+          {/if}
         </div>
       </Tooltip.Content>
     </Tooltip.Root>
