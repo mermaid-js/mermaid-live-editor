5ed07028c7bb5a4910e9aef21888a163088361ee
Sidharth Vinod <sidharthv96@gmail.com>
2025-03-17 20:04:15 -0700

    chore: Ignore cypress

--- vite.config.js
+++ vite.config.js
@@ -23,6 +23,7 @@ export default defineConfig({
       'tests/**/*',
       '**\/node_modules/**',
       '**\/dist/**',
+      '**\/cypress/**',
       '**\/.{idea,git,cache,output,temp}/**',
       '**\/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build,eslint,prettier}.config.*'
     ],
